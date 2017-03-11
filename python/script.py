66ca1d0a5c33240914db65ecdf372edf5278f1f8
Gyorgy Rethy <gyorgy.rethy@student.manchester.ac.uk>
2017-03-11 16:52:08 +0000

    registration

--- python/script.py
+++ python/script.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+import os
+import json
+from flask import Flask, request, session, g
+from flaskext.mysql import MySQL
 
-app = Flask(__name__)
+mysql = MySQL()
+app = Flask(__name__) #create app instance
+app.config['MYSQL_DATABASE_USER'] = 'hackathon'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'hackathon'
+app.config['MYSQL_DATABASE_DB'] = 'doee'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * FROM employer")
+    data = cursor.fetchall()
+    cursor.close()
+    return json.dumps(data)
+    
+@app.route("/registerEmployer")
+def registerEmployer():
+    
+    firstname = "default"
+    lastname = "default"
+    username = str(request.args.get('UserName'))
+    password = str(request.args.get('Password'))
+    email = "default"
+    phonenumber = "123456789"
+    address = "default"
+    
+    sql = "insert into employer values('0','"+firstname+"','"+lastname+"','"+username+"','"+password+"','"+email+"','"+phonenumber+"','"+address+"');"
+    
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    
+    return "Registration successful"
 
 if __name__ == "__main__":
     app.run()
